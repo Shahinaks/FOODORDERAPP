e9a4e761425cce8aa458ce61cf1005a22f67ed48
Shahinaks <shahina.sharafudeenki@gmail.com>
2025-07-06 14:44:05 +0530

    Update firebaseAdmin.js

--- Server/firebase/firebaseAdmin.js
+++ Server/firebase/firebaseAdmin.js
@@ -1,11 +1,9 @@
-
 import admin from 'firebase-admin';
 import { readFileSync } from 'fs';
 
-import path from 'path';
-
-const serviceAccountPath = path.resolve('firebase', 'firebase-adminsdk.json');
+const serviceAccountPath = '/etc/secrets/firebase-adminsdk.json';
 const serviceAccount = JSON.parse(readFileSync(serviceAccountPath, 'utf-8'));
+
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
