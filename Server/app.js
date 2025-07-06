99c35288dc8ad9bf5594d21ffbbb5d007e9e5b1a
Shahinaks <shahina.sharafudeenki@gmail.com>
2025-07-06 20:02:32 +0530

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -28,11 +28,19 @@ const allowedOrigins = [
 
 
 const corsOptions = {
-  origin: allowedOrigins,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.error('❌ CORS blocked request from:', origin);
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
+
 
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
