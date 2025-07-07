a2f1bacbe4f0ddc71bdcf0a54e12c6822b433381
Shahinaks <shahina.sharafudeenki@gmail.com>
2025-07-07 20:38:16 +0530

    Update LoginPage.jsx

--- Client/src/Pages/LoginPage.jsx
+++ Client/src/Pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Button, Alert, Spinner } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import {
   signInWithEmailAndPassword,
   signInWithPopup,
